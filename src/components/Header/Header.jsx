83cba807c333a46810a4d20cf3fa7ce1f0c7c4dc
StarCatcher23 <dreamandachievetoday@gmail.com>
2026-04-24 21:39:08 -0400

    different between authorized and non-authorized navigation bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import { NavLink, useLocation } from "react-router-dom";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import "./Header.css";
 import logo from "../../assets/logo.svg";
-import avatar from "../../assets/avatar.svg";
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function Header({
   handleAddClick,
@@ -12,14 +13,18 @@ function Header({
   isLoggedIn,
 }) {
   const location = useLocation();
-
-  const userLinkTarget = "/profile";
+  const currentUser = useContext(CurrentUserContext);
 
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
     year: "numeric",
   });
+
+  const getInitial = (name) => {
+    if (!name) return "?";
+    return name.charAt(0).toUpperCase();
+  };
 
   return (
     <header className="header">
@@ -33,40 +38,55 @@ function Header({
 
       <ToggleSwitch />
 
-      <button
-        onClick={() => {
-          if (!isLoggedIn) {
-            alert("Please log in first");
-            return;
-          }
-          handleAddClick();
-        }}
-        type="button"
-        className="header__add-clothes-btn"
-      >
-        + Add Clothes
-      </button>
+      {/* AUTHORIZED USER UI */}
+      {isLoggedIn ? (
+        <>
+          <button
+            onClick={handleAddClick}
+            type="button"
+            className="header__add-clothes-btn"
+          >
+            + Add Clothes
+          </button>
 
-      <button
-        onClick={onRegisterClick}
-        type="button"
-        className="header__auth-btn"
-      >
-        Sign Up
-      </button>
+          <NavLink to="/profile" className="header__nav-link">
+            <div className="header__user-container">
+              <p className="header__username">{currentUser?.name}</p>
 
-      <button onClick={onLoginClick} type="button" className="header__auth-btn">
-        Log In
-      </button>
+              {currentUser?.avatar ? (
+                <img
+                  src={currentUser.avatar}
+                  alt={currentUser.name}
+                  className="header__avatar"
+                />
+              ) : (
+                <div className="header__avatar-placeholder">
+                  {getInitial(currentUser?.name)}
+                </div>
+              )}
+            </div>
+          </NavLink>
+        </>
+      ) : (
+        /* NON-AUTHORIZED USER UI */
+        <>
+          <button
+            onClick={onRegisterClick}
+            type="button"
+            className="header__auth-btn"
+          >
+            Sign Up
+          </button>
 
-      <NavLink to="/profile" className="header__profile-link"></NavLink>
-
-      <NavLink to={userLinkTarget} className="header__nav-link">
-        <div className="header__user-container">
-          <p className="header__username">Terrence Tegegne</p>
-          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
-        </div>
-      </NavLink>
+          <button
+            onClick={onLoginClick}
+            type="button"
+            className="header__auth-btn"
+          >
+            Log In
+          </button>
+        </>
+      )}
     </header>
   );
 }
